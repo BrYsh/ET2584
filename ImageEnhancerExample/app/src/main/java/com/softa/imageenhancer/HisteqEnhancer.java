37b17615d5bc3f99de11af2877e30d42b13727be
BrYsh <andreas.brorsson6@gmail.com>
2016-09-09 13:06:13 +0200

    Multithread

--- ImageEnhancerExample/app/src/main/java/com/softa/imageenhancer/HisteqEnhancer.java
+++ ImageEnhancerExample/app/src/main/java/com/softa/imageenhancer/HisteqEnhancer.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap.Config;
 import android.graphics.Color;
 import android.util.Log;
 
+
+
 public class HisteqEnhancer implements ImageEnhancer {
 
 	private int progress;
@@ -14,6 +16,7 @@ public class HisteqEnhancer implements ImageEnhancer {
 	}
 
 	// Att göra: testa Texten, fixa någon start i histeq att testa
+
 
 	public Bitmap enhanceImageHSV(Bitmap theImage, int action) {
 
@@ -63,7 +66,7 @@ public class HisteqEnhancer implements ImageEnhancer {
 		float bitdepth = (float)255;
 		float scalar = bitdepth/N;
 
-		float[] hist_array_cs = new float[256];
+		final float[] hist_array_cs = new float[256];
 		hist_array_cs[0] = (float)hist_array[0]*scalar;
 
 		// kumulativa fördelningsfunktionen
@@ -73,16 +76,87 @@ public class HisteqEnhancer implements ImageEnhancer {
 		progress = 70;
 
 		// Transformera tillbaka
-		float old_i,new_i;
-		for (int i = 0; i < hsvPixels.length; i++) {
-			old_i = hsvPixels[i][2];
-			new_i = hist_array_cs[ Math.round( old_i*(float)255 ) ]/(float)255;
+		float old_i,new_i,f255 = 255;
+		final int pl = hsvPixels.length/3;
+		final int pl2 = pl*2;
+		final int pl3 = hsvPixels.length;
 
-			hsvPixels[i][2] = new_i;
+		final float[][] hsvPixelsT = hsvPixels;
+		final int[] pixelsT = pixels;
+		//Loop1
+		Thread t1 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				float old_i,new_i,f255 = 255;
+				System.out.println("1 loop start: TB");
+				for (int i = 0; i < pl; i++) {
+					old_i = hsvPixelsT[i][2];
+					new_i = hist_array_cs[ Math.round( old_i*f255 ) ]/f255;
 
-			//Konvertera tillbaka till RGB
-			pixels[i] = Color.HSVToColor(hsvPixels[i]);
+					hsvPixelsT[i][2] = new_i;
+
+					//Konvertera tillbaka till RGB
+					pixelsT[i] = Color.HSVToColor(hsvPixelsT[i]);
+				}
+			}
+		});
+
+		t1.setPriority(10);
+		t1.start();
+
+		//Loop1
+		Thread t2 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				float old_i,new_i,f255 = 255;
+				System.out.println("2 loop start: TB");
+				for (int i = pl; i < pl2; i++) {
+					old_i = hsvPixelsT[i][2];
+					new_i = hist_array_cs[ Math.round( old_i*f255 ) ]/f255;
+
+					hsvPixelsT[i][2] = new_i;
+
+					//Konvertera tillbaka till RGB
+					pixelsT[i] = Color.HSVToColor(hsvPixelsT[i]);
+				}
+			}
+		});
+
+		t2.setPriority(10);
+		t2.start();
+
+		//Loop1
+		Thread t3 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				float old_i,new_i,f255 = 255;
+				System.out.println("3 loop start: TB");
+				for (int i = pl2; i < pl3; i++) {
+					old_i = hsvPixelsT[i][2];
+					new_i = hist_array_cs[ Math.round( old_i*f255 ) ]/f255;
+
+					hsvPixelsT[i][2] = new_i;
+
+					//Konvertera tillbaka till RGB
+					pixelsT[i] = Color.HSVToColor(hsvPixelsT[i]);
+				}
+			}
+		});
+
+		t3.setPriority(10);
+		t3.start();
+
+		while(t1.isAlive() ||t2.isAlive()||t3.isAlive()){
+			continue;
 		}
+
+
+		pixels = pixelsT;
+		hsvPixels = hsvPixelsT;
+
 		progress = 95;
 
 	}
@@ -91,24 +165,158 @@ public class HisteqEnhancer implements ImageEnhancer {
 
 	public int[] make_histogram(float[][] hsvPixels){
 
-		int[] hist_array = new int[256];
-		int intens;
-		for (int i = 0; i < hsvPixels.length; i++) {
-			intens = Math.round( hsvPixels[i][2]*255 );
+		final int[] hist_array = new int[256];
 
-			hist_array[ intens ] += 1;
 
+		final float[][] hsvPixelsT = hsvPixels;
+		final int pl = hsvPixels.length/3;
+		final int pl2 = pl*2;
+		final int pl3 = hsvPixels.length;
+
+		//Loop1
+		Thread t1 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				int intens;
+				System.out.println("1 loop start: Make hist");
+				for (int i = 0; i < pl; i++) {
+					intens = Math.round( hsvPixelsT[i][2]*255 );
+
+					hist_array[ intens ] += 1;
+				}
+			}
+		});
+
+		t1.setPriority(10);
+		t1.start();
+
+		//Loop2
+		Thread t2 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				int intens;
+				System.out.println("2 loop start: Make hist");
+				for (int i = pl; i < pl2; i++) {
+					intens = Math.round( hsvPixelsT[i][2]*255 );
+
+					hist_array[ intens ] += 1;
+				}
+			}
+		});
+
+		t2.setPriority(10);
+		t2.start();
+
+		//Loop2
+		Thread t3 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				int intens;
+				System.out.println("3 loop start: Make hist");
+				for (int i = pl2; i < pl3; i++) {
+					intens = Math.round( hsvPixelsT[i][2]*255 );
+
+					hist_array[ intens ] += 1;
+				}
+			}
+		});
+
+		t3.setPriority(10);
+		t3.start();
+
+		while(t1.isAlive() ||t2.isAlive()||t3.isAlive()){
+			continue;
 		}
 
 		return hist_array;
 
 	}
 
+
+
+
+
 	private float[][] convertToHSV(int[] pixels) {
+
+		final float[][] hsvPixels = new float[pixels.length][3];
+		final int[] pixelsth = pixels;
+
+		final int pl = pixelsth.length/3;
+		final int pl2 = pl*2;
+		final int pl3 = pixelsth.length;
+		//final int pl4 = pixelsth.length;
+
+		//Loop1
+		Thread t1 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				System.out.println("1 loop start");
+				for (int i = 0; i < pl; i++) {
+					Color.RGBToHSV(Color.red(pixelsth[i]), Color.green(pixelsth[i]), Color.blue(pixelsth[i]), hsvPixels[i]);
+
+				}
+			}
+		});
+
+		t1.setPriority(10);
+		t1.start();
+
+		//Loop2
+		Thread t2 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				System.out.println("2 loop start");
+				for (int i = pl; i < pl2; i++) {
+					Color.RGBToHSV(Color.red(pixelsth[i]), Color.green(pixelsth[i]), Color.blue(pixelsth[i]), hsvPixels[i]);
+
+				}
+			}
+		});
+
+		t2.setPriority(10);
+		t2.start();
+
+		//Loop1
+		Thread t3 = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				System.out.println("3 loop start");
+				for (int i = pl2; i < pl3; i++) {
+					Color.RGBToHSV(Color.red(pixelsth[i]), Color.green(pixelsth[i]), Color.blue(pixelsth[i]), hsvPixels[i]);
+
+				}
+			}
+		});
+
+		t3.setPriority(9);
+		t3.start();
+
+
+
+		while(t1.isAlive() || t2.isAlive() || t3.isAlive()){
+			continue;
+		}
+
+		return hsvPixels;
+	}
+
+
+
+
+
+
+
+	private float[][] convertToHSV2(int[] pixels) {
 		float[][] hsvPixels = new float[pixels.length][3];
 		for (int i = 0; i < pixels.length; i++) {
 			Color.RGBToHSV(Color.red(pixels[i]), Color.green(pixels[i]), Color.blue(pixels[i]), hsvPixels[i]);
-			
+
 		}
 		return hsvPixels;
 	}
@@ -131,3 +339,17 @@ public class HisteqEnhancer implements ImageEnhancer {
 	}
 
 }
+
+
+//public class rgb2hsvThread extends Thread {
+//
+//	public float[][] run(int[] pixels){
+//		System.out.println("MyThread running");
+//		float[][] hsvPixels = new float[pixels.length][3];
+//		for (int i = 0; i < pixels.length; i++) {
+//
+//
+//		}
+//		return Color.RGBToHSV(Color.red(pixels[i]), Color.green(pixels[i]), Color.blue(pixels[i]), hsvPixels[i]);;
+//	}
+//}
